a1b84ad061e5d475e2b5fead85206c848de57a1d
takagotch <dyaccb@gmail.com>
2018-02-10 08:59:46 +0900

    view.appendTo('body');

--- js/thorax/app.js
+++ js/thorax/app.js
@@ -1,3 +1,22 @@
+var subview = new Thorax.View({
+  model: new Thorax.Model({
+    description: '...',
+    author: '...'
+  }),
+  label: 'Title',
+  context: function(){
+    return{
+      bookname: this.model.get('bookname').toUpperCase()
+    }
+  },
+  subview: subview,
+//template: Handlebars.compile("{{description}}"),
+  template: Handlebars.compile(
+    "{{;abe;}} : {{bookname}} by {{author}} <br> {{view subview}}"),
+});
+view.appendTo('body');
+
+
 var timeline - new com.apress.collectin.thorax.Timeline();
 
 timeline.fetch({reset:tru, success: function(){
@@ -6,4 +25,5 @@ timeline.fetch({reset:tru, success: function(){
   });
   timelineView.appendTo('#timeline-section');
 }});
+
 
