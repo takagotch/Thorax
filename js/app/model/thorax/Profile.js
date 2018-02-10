9cc579997c114664f0cc92338270a2d76516e40e
takagotch <dyaccb@gmail.com>
2018-02-10 21:34:59 +0900

    com.apress.model.thorax = com.apress.model.thorax || {}l

---
+++ js/app/model/thorax/Profile.js
@@ -0,0 +1,14 @@
+var com = com || {};
+com.apress = com.apress.model || {};
+com.apress = com.apress.model || {};
+com.apress.model.thorax = com.apress.model.thorax || {};
+
+com.apress.model.thorax.Profile = Thorax.Model.extend({
+  
+  urlRoot: 'http://localhost:8080/profile',
+
+  parse: function(model){
+    return model;
+  }
+});
+
--- js/thorax/app.js
+++ js/thorax/app.js
@@ -1,3 +1,5 @@
+
+
 var subview = new Thorax.View({
   model: new Thorax.Model({
     description: '...',
@@ -17,7 +19,27 @@ var subview = new Thorax.View({
 view.appendTo('body');
 
 
-var timeline - new com.apress.collectin.thorax.Timeline();
+
+var layout = new Thorax.LayoutView();
+layout.appendTo('body');
+
+var view = new Thorax.View({
+  events: {
+    ready: function(){console.log('View is ready');},
+    destroyed: function(){console.log('View is destroyed');},
+    rendered: function(){console.log('View is rendered');}
+  },
+  model: new Thorax.Model({
+    bookname: '',
+    author: ''
+  }),
+  template: Handlebars.compile("{{bookname}} by {{author}}")
+});
+layout.setView(view);
+
+
+
+var timeline = new com.apress.collectin.thorax.Timeline();
 
 timeline.fetch({reset:tru, success: function(){
   var timelineView = new com.apress.view.ThoraxTimelineView({
